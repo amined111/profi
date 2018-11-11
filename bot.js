fad25572663d2187dbe14346c362662e23857698
amined111 <43893689+amined111@users.noreply.github.com>
2018-11-11 23:06:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -390,4 +390,39 @@ client.on('voiceStateUpdate', (voiceOld, voiceNew) => {
         logChannel.send(voiceLeave);
     }
 });
+const coolDown = new Set();
+client.on('message', message => {
+  
+      if (message.content.startsWith("رابط")) {
+        if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your invite :yen: link refreshes in \`\`1 Day\`\`.**`);
+
+    message.channel.createInvite({
+  
+          thing: true,
+  
+          maxUses: 5,
+  
+          maxAge: 86400
+  
+      }).then(invite =>
+  
+        message.author.sendMessage(invite.url)
+  
+      )
+  
+    message.channel.send("**تم ارسال الرابط برسالة خاصة**")   .then(() => {     
+      coolDown.add(message.author.id);
+  });
+  
+  
+  message.author.send(`**مدة الرابط : يـوم
+  عدد استخدامات الرابط : 5**`)
+  
+      }
+
+      setTimeout(() => {
+        coolDown.remove(message.author.id);
+     },86400000);
+     
+  });
 client.login(process.env.BOT_TOKEN);
