06ab529d6afdbfe0e7056e9ed3a46b81197a06ed
amined111 <43893689+amined111@users.noreply.github.com>
2018-11-11 23:14:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -425,4 +425,33 @@ client.on('message', message => {
      },86400000);
      
   });
+<@505102973152526366>
+
+    client.on('message', message => { //  by [ <@480407581085532180> ]
+
+    if(message.content.startsWith(prefix + 'move all')) {
+
+     if (!message.member.hasPermission("MOVE_MEMBERS")) return message.channel.send('**لايوجد لديك صلاحية سحب الأعضاء**');
+
+     if(!message.guild.member(client.user).hasPermission("MOVE_MEMBERS")) return message.reply("**لايوجد لدي صلاحية السحب**");
+
+    if (message.member.voiceChannel == null) return message.channel.send(`**الرجاء الدخول لروم صوتي**`)
+
+     var author = message.member.voiceChannelID;
+
+     var m = message.guild.members.filter(m=>m.voiceChannel)
+
+     message.guild.members.filter(m=>m.voiceChannel).forEach(m => {
+
+     m.setVoiceChannel(author)
+
+     })
+
+     message.channel.send(`**تم سحب جميع الأعضاء الي الروم الصوتي حقك.**`)
+
+
+
+     }
+
+     });
 client.login(process.env.BOT_TOKEN);
