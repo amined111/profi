db72727d235548d1d502355bb5fa566399be0bc9
amined111 <43893689+amined111@users.noreply.github.com>
2018-11-12 00:57:15 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,7 +71,7 @@ message.author.send(`**مدة الرابط : يـوم
 
 client.on('message', msg => {
   if (msg.author.bot) return;
-  if (!msg.content.startsWith()) return;
+  if (!msg.content.startsWith($)) return;
   let command = msg.content.split(" ")[0];
   command = command.slice(prefix.length);
   let args = msg.content.split(" ").slice(1);
