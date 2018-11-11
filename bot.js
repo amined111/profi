c30456ce2434174eeee13145e60c6c5c818f9a04
amined111 <43893689+amined111@users.noreply.github.com>
2018-11-11 23:51:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
 client.on('guildMemberAdd', member => {
     var embed = new Discord.RichEmbed()
     .setThumbnail(member.user.avatarURL)
@@ -96,56 +95,7 @@ client.on('channelUpdate', (oldChannel, newChannel) => {
         }
     })
 });
- 
-client.on('guildBanAdd', (guild, user) => {
- 
-    if(!guild.member(client.user).hasPermission('EMBED_LINKS')) return;
-    if(!guild.member(client.user).hasPermission('VIEW_AUDIT_LOG')) return;
- 
-    var logChannel = guild.channels.find(c => c.name === 'log');
-    if(!logChannel) return;
- 
-    guild.fetchAuditLogs().then(logs => {
-        var userID = logs.entries.first().executor.id;
-        var userAvatar = logs.entries.first().executor.avatarURL;
- 
-        if(userID === client.user.id) return;
- 
-        let banInfo = new Discord.RichEmbed()
-        .setTitle('**[BANNED]**')
-        .setThumbnail(userAvatar)
-        .setColor('DARK_RED')
-        .setDescription(`**\n**:airplane: Successfully \`\`BANNED\`\` **${user.username}** From the server!\n\n**User:** <@${user.id}> (ID: ${user.id})\n**By:** <@${userID}> (ID: ${userID})`)
-        .setTimestamp()
-        .setFooter(guild.name, guild.iconURL)
- 
-        logChannel.send(banInfo);
-    })
-});
-client.on('guildBanRemove', (guild, user) => {
-    if(!guild.member(client.user).hasPermission('EMBED_LINKS')) return;
-    if(!guild.member(client.user).hasPermission('VIEW_AUDIT_LOG')) return;
- 
-    var logChannel = guild.channels.find(c => c.name === 'log');
-    if(!logChannel) return;
- 
-    guild.fetchAuditLogs().then(logs => {
-        var userID = logs.entries.first().executor.id;
-        var userAvatar = logs.entries.first().executor.avatarURL;
- 
-        if(userID === client.user.id) return;
- 
-        let unBanInfo = new Discord.RichEmbed()
-        .setTitle('**[UNBANNED]**')
-        .setThumbnail(userAvatar)
-        .setColor('GREEN')
-        .setDescription(`**\n**:unlock: Successfully \`\`UNBANNED\`\` **${user.username}** From the server\n\n**User:** <@${user.id}> (ID: ${user.id})\n**By:** <@${userID}> (ID: ${userID})`)
-        .setTimestamp()
-        .setFooter(guild.name, guild.iconURL)
- 
-        logChannel.send(unBanInfo);
-    })
-});
+
 client.on('guildUpdate', (oldGuild, newGuild) => {
  
     if(!oldGuild.member(client.user).hasPermission('EMBED_LINKS')) return;
@@ -426,35 +376,6 @@ client.on('message', message => {
      },86400000);
      
   });
-var antispam = require("anti-spam");//npm i anti-spam
- 
-antispam(client, {
-  warnBuffer: 3, //الحد الأقصى المسموح به من الرسائل لإرسالها في الفاصل الزمني قبل الحصول على تحذير.
-  maxBuffer: 5, // الحد الأقصى المسموح به من الرسائل لإرسالها في الفاصل الزمني قبل الحصول على ميوت.
-  interval: 1000, // مقدار الوقت قبل حصول باند
-  warningMessage: "stop spamming.", // رسالة تحذير اذا سوا سبام!
-  roleMessage: "Muted!!", // الرسالة الي تجي اذا شخص اخذ ميوت
-  roleName: "Muted", // اسم رتبة الميوت
-  maxDuplicatesWarning: 7, // عدد الرسايل الي قبل التحذيرات
-  maxDuplicatesBan: 10, // عدد الرسايل الي يقدر المستخدم يرسلها قبل الميوت
-  time: 10, // عدد الوقت الي يجلس لين تسحب رتبة الميوت من الشخص الحسبة برمجية وليست كتابية 
-});
-client.on('message',async message => {
-  if(message.content.startsWith("!setvoice")) {
-  if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
-  if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
-  message.channel.send('✅| **تم عمل الروم بنجاح**');
-  message.guild.createChannel(`Voice Online : [ ${message.guild.members.filter(m => m.voiceChannel).size} ]` , 'voice').then(c => {
-    console.log(`Voice online channel setup for guild: \n ${message.guild.name}`);
-    c.overwritePermissions(message.guild.id, {
-      CONNECT: false,
-      SPEAK: false
-    });
-    setInterval(() => {
-      c.setName(`Voice Online : [ ${message.guild.members.filter(m => m.voiceChannel).size} ]`)
-    },1000);
-  });
-  }
-});
+
 
 client.login(process.env.BOT_TOKEN);
