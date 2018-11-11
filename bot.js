dedd7de32f1de0ada1f8f5eb759d597a1ca7bd9c
amined111 <43893689+amined111@users.noreply.github.com>
2018-11-12 00:46:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "-";
+var prefix = "$";
 client.on('message', message => { // Leaked by [ @Out Our server ]
    if(!message.channel.guild) return;
 if(message.content.startsWith(prefix + 'bc')) {
@@ -50,7 +50,7 @@ msg.delete();
 });
 
   client.on('message', message => {
-    if (message.content.startsWith("رابط")) {
+    if (message.content.startsWith("$Link")) {
 
   message.channel.createInvite({
         thing: true,
