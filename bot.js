e4503f009f891b2781073b6442c748926ed59dbd
amined111 <43893689+amined111@users.noreply.github.com>
2018-11-12 20:31:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -748,7 +748,25 @@ client.on('ready', () => {
 client.user.setGame(`Sylight|#help .`,"http://twitch.tv/aminn")
 client.user.setStatus("dnd")
 });
- 
+client.on('message', message => {
+    if (!message.channel.guild) return;
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply("***  ليس معك صلاحيات  ***")
+    var prefix = "#";
+       if (message.content ===   prefix + "add role") {
+    message.channel.send("<@" + message.author.id + ">  ***  جاري اعطاء الرتبة للاعضاء كما امرت  *** ")
+    message.guild.members.forEach(m => {
+    m.addRole(message.guild.roles.find('name', 'Light'))
+    })
+    }
+    
+       if (message.content ===   prefix + "remove role") {
+    message.channel.send("<@" + message.author.id + ">  ***  جاري اعطاء الرتبة للاعضاء كما امرت  *** ")
+    message.guild.members.forEach(m => {
+    m.removeRole(message.guild.roles.find('name', 'Light'))
+    })
+    }
+    
+    }); 
  
  
 client.on('message' , message => {
