61ea7a8053ef9bd3c21c17a9edb247583a476f43
amined111 <43893689+amined111@users.noreply.github.com>
2018-11-12 01:18:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -68,6 +68,17 @@ message.author.send(`**مدة الرابط : يـوم
     }
 });
 
+client.on('guildMemberAdd', member => {
+    var embed = new Discord.RichEmbed()
+    .setThumbnail(member.user.avatarURL)
+  .addField("***شكرا الانضمامك الينا***" ,member.user.username )
+    .setDescription('***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا***')
+    .setColor('RANDOM')
+    .setImage('http://www.imgion.com/images/01/Welcome-buddy.jpg')
+var channel =member.guild.channels.find('name', 'chat')
+if (!channel) return;
+channel.send({embed : embed});
+});
 
 client.on('message', msg => {
   if (msg.author.bot) return;
